521325ca6769e4b87a43628fd6c59cf237e567df
jayd3nkang <jjk582005@gmail.com>
2024-04-20 19:02:08 -0400

    made it able to control it in main

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class Main extends JPanel{
+  public static void main(String[] args){
+    JFrame frame = new JFrame();
+    Home ex = new Home();
+    frame.setContentPane(ex);
+    frame.pack();
+    frame.setVisible(true);
+    }
+}
