0f5abdf2c04223342e4e458c85b7d40c2ae22c49
jayd3nkang <jjk582005@gmail.com>
2024-05-07 01:14:46 -0400

    facts again

--- Goods.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Graphics;
-
-public interface Goods {
-  public void draw(Graphics g);
-  public int isCompostable();
-  public int getClassification();
-  public void setClassification(int c);
-  public String toString();
-}
