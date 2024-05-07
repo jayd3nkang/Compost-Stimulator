e854cf865560f3f8be86e0a67da6e65f7d21a363
EllieNYang <166646173+EllieNYang@users.noreply.github.com>
2024-05-07 00:46:44 -0400

    small things

--- flower.java
+++ flower.java
@@ -23,34 +23,34 @@ public class flower extends Main{
     public void drawSprout(Graphics g){
         ImageIcon plant = new ImageIcon("flower1(1).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+200, this);
     }
     public void drawSproutS2(Graphics g){
         ImageIcon plant = new ImageIcon("flower1(2).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+200, this);
     }
     public void drawSproutS3(Graphics g){
         ImageIcon plant = new ImageIcon("flower1(3).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+200, this);
     }
 
 
     public void drawFlowerS2(Graphics g){
         ImageIcon plant = new ImageIcon("flower2(1).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+170, this);
     }
     public void drawFlowerS2S2(Graphics g){
         ImageIcon plant = new ImageIcon("flower2(2).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+170, this);
     }
     public void drawFlowerS2S3(Graphics g){
         ImageIcon plant = new ImageIcon("flower2(3).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+170, this);
     }
     
 
@@ -59,66 +59,66 @@ public class flower extends Main{
     public void drawFlowerS3(Graphics g){
         ImageIcon plant = new ImageIcon("flower3(1).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y ,this);
+        g.drawImage(flower, x, y+150,this);
     }
     public void drawFlowerS3S2(Graphics g){
         ImageIcon plant = new ImageIcon("flower3(2).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y ,this);
+        g.drawImage(flower, x, y+150,this);
     }
     public void drawFlowerS3S3(Graphics g){
         ImageIcon plant = new ImageIcon("flower3(3).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y ,this);
+        g.drawImage(flower, x, y+150,this);
     }
 
 
     public void drawFlowerS4(Graphics g){
         ImageIcon plant = new ImageIcon("flower4(1).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+30, this);
     }
     public void drawFlowerS4S2(Graphics g){
         ImageIcon plant = new ImageIcon("flower4(2).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+30, this);
     }
     public void drawFlowerS4S3(Graphics g){
         ImageIcon plant = new ImageIcon("flower4(3).png");
         Image flower = plant.getImage();
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+30, this);
     }
 
     public void drawHealthy(Graphics g){
         ImageIcon plant = new ImageIcon("flower5(1).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+10, this);
     }
     public void drawHealthyS2(Graphics g){
         ImageIcon plant = new ImageIcon("flower5(2).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+10, this);
     }
     public void drawHealthyS3(Graphics g){
         ImageIcon plant = new ImageIcon("flower5(3).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+10, this);
     }
 
 
     public void drawUnhealthy(Graphics g){
         ImageIcon plant = new ImageIcon("deadFlower(3).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+40, this);
     }
     public void drawUnhealthyS2(Graphics g){
         ImageIcon plant = new ImageIcon("deadFlower(2).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+40, this);
     }
     public void drawUnhealthyS3(Graphics g){
         ImageIcon plant = new ImageIcon("deadFlower(1).png");
         Image flower = plant.getImage();   
-        g.drawImage(flower, x, y, this);
+        g.drawImage(flower, x, y+40, this);
     }
 }
